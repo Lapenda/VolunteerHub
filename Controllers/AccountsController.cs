ecde3501bd74bd6538bd3244bf14ec096d086b78
lapenda <borna.lapenda12@gmail.com>
2026-03-28 00:14:55 +0100

    Initial commit

---
+++ Controllers/AccountsController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace VolunteerHub.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountsController : ControllerBase
+    {
+        
+    }
+}
